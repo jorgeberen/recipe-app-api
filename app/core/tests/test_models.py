2f2a552eab91912d0f8f4d547be2bd33d699bfb5
Jorge Berenguer <jorge.beren@gmail.com>
2022-09-18 21:50:03 +0100

    added admin user tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -23,9 +23,26 @@ class ModelTests(TestCase):
         """Test the email for a new user is normalized"""
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test2@example.com', 'test2@example.com']
+            ['Test2@example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.com', 'test4@example.com']
         ]
 
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test creating user without email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'sample123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
