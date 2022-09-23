c56794db712ae8dc20f9b6af62b89c14e482e2fa
Jorge Berenguer <jorge.beren@gmail.com>
2022-09-23 16:05:31 +0100

    added test to create user page in django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -12,20 +12,34 @@ class UserAdmin(BaseUserAdmin):
     ordering = ('id',)
     list_display = ("email", "name")
     fieldsets = (
-        (None, {'fields': {'email', 'password'}}),
+        (None, {'fields': ('email', 'password')}),
         (
             _('Permissions'),
             {
                 'fields': (
                     'is_active',
                     'is_staff',
-                    'is_superuser'
+                    'is_superuser',
                 )
             }
         ),
-        (_('Important dates'), {'fields': ('last_login',)})
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ("wide", ),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
